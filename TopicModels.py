a95f7653323ea2be4808f0e4b2e0a91dffcdab5c
Kush Mittal <Kush@WinterIsComing.local>
2017-04-05 22:29:03 -0500

    Adding E step for EM function

--- TopicModels.py
+++ TopicModels.py
@@ -1,5 +1,6 @@
 import numpy
 import pandas
+import math
 
 # For kmeans
 from sklearn.cluster import KMeans
@@ -10,11 +11,18 @@ num_words = 12419
 # Number of clusters
 num_topics = 30
 
-def get_word_prob_vectors(docs):
+def get_model_parameters(docs):
 	p = numpy.array([numpy.array([float(0) for j in range(num_words+1)]) for i in range(num_topics)])
+	pi = numpy.array([float(0) for i in range(num_topics)])
 
 	kmeans_p = KMeans(n_clusters=num_topics).fit(docs)
 	labels =  kmeans_p.labels_
+
+	# Normalize to make elements probabilities
+	for k in labels:
+		pi[k] = pi[k] + 1
+	for k in range(len(pi)):
+		pi[k] = float(pi[k])/len(labels)
 	
 	# Add the vectors for each topic, and avoid 0's
 	for x in range(len(labels)):
@@ -30,7 +38,34 @@ def get_word_prob_vectors(docs):
 			p[i][j] = float(p[i][j])/s
 			assert p[i][j] != 0
 
-	return p
+	return p, pi
+
+def e_step(p, pi, docs):
+	# Compute z vectors
+	z = numpy.dot(docs, numpy.transpose(p))
+
+	for i in range(len(z)):
+		for j in range(len(z[0])):
+			z[i][j] = z[i][j] + numpy.log(pi[j])
+
+	w = numpy.copy(z)
+
+	# Compute w matrix
+	for i in range(len(w)):
+		m = max(w[i])
+			
+		w[i] = numpy.power(w[i] - m, math.e)
+		w[i] = w[i]/float(sum(w[i]))
+
+	# Compute the Q value
+	print numpy.sum(numpy.multiply(z, w))
+
+	return p, pi, w
+
+def m_step(p, pi, w, docs):
+	# Recompute p vectors
+
+	for i in range(num_documents+1)
 
 def main():
 	# Read in the vociabulary
@@ -47,12 +82,15 @@ def main():
 	# Estimate Model Parameters
 
 	# p[i][j] represents the probability that word j is in topic i
-	p = get_word_prob_vectors(docs)
-	
 	# pi[i] represent the probability a given document is in topic i
-	pi = numpy.array([float(i)/(15*31) for i in range(num_topics)])
+	p, pi = get_model_parameters(docs)
 	
+	# Compute Labels (E step)
+	p, pi, w = e_step(p, pi, docs)
 
+	m_step(p, pi, w, docs)
+
+	# Recompute parameters (M Step)
 	
 	
 if __name__ == '__main__':
